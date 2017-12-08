c5b4af979e99f6ae97415a282ab04e46db6b29bc
Bryan Nguyen <nguyenbryann@gmail.com>
2017-12-08 14:36:24 -0500

    dada2 filtering/trimming now works and rename_postQC.R script added

--- dada2_analysis.R
+++ dada2_analysis.R
@@ -1,8 +1,8 @@
 library(dada2)
 
-setwd("~/Documents/Grad/STRI/prototyping/scripts")
 BCS_list <- list.dirs('..')[grep("BCS", list.dirs('..'))]
 #We will create or use an existing directory called 'filtered' in each BCS folder
+#Move Undetermined or unassigned sequences to a subdirectory.
 filt_paths <- file.path(BCS_list, 'filtered')
 #Maybe loop starting here
 #BCS1_files = list.files(path = BCS_list[1], full.names = TRUE)
@@ -11,10 +11,10 @@ filt_paths <- file.path(BCS_list, 'filtered')
 BCS_fnFs <- lapply(BCS_list, function(x) sort(list.files(x, pattern="_1.fastq", full.names = TRUE)))
 BCS_fnRs <- lapply(BCS_list, function(x) sort(list.files(x, pattern="_2.fastq", full.names = TRUE)))
 
-sample.names.1 <- c('a','b','c','d') #fix this with real sample names later
+sample.names.1 <- c('a','b') #fix this with real sample names later
 #plotQualityProfile(BCS1_fnFs[1:2]) #trim last ~2-5  nucleotides?
 #plotQualityProfile(BCS1_fnRs[1:2]) #Could trim about ~5-10 also
 BCS1_filtFs <- file.path(filt_paths[1], paste0(sample.names.1, '_F_filt.fastq.gz'))
 BCS1_filtRs <- file.path(filt_paths[1], paste0(sample.names.1, '_R_filt.fastq.gz'))
 
-BCS1_out <- filterAndTrim(unlist(BCS_fnFs[1]), BCS1_filtFs, unlist(BCS_fnRs[1]), BCS1_filtRs, rm.phix = TRUE, maxN = 0, maxEE = c(2,2), truncQ = 10, trimLeft = 32, truncLen = c(245,243), multithread = FALSE) #set multithread to TRUE when running in normal R and not RStudio
+BCS1_out <- filterAndTrim(unlist(BCS_fnFs[1])[c(34,19)], BCS1_filtFs, unlist(BCS_fnRs[1])[c(34,19)], BCS1_filtRs, rm.phix = TRUE, maxN = 0, maxEE = c(2,2), truncQ = 10, trimLeft = 32, multithread = TRUE) #set multithread to TRUE when running in normal R and not RStudio
