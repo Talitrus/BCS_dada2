4891ffa4325684430cd742d1bc4b6ee812776ea4
Talitrus <nguyenbryann@gmail.com>
2018-01-22 15:12:22 -0500

    revised to include bash script to run R code for renaming, added to README and updated phyloseq.sh to run correctly named R script

--- rename.sh
+++ rename.sh
@@ -3,10 +3,10 @@
 #SBATCH -o out_err_files/R_%A.out
 #SBATCH -e out_err_files/R_%A.err
 #SBATCH --nodes=1
-#SBATCH -t 14-00:00:00
-#SBATCH -p defq
+#SBATCH -t 02:00:00
+#SBATCH -p defq,short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=bnguyen@gwu.edu
 
 module load R/3.4.2
-Rscript BCS_phyloseq.R
+Rscript rename_postQC.R
