c9b7850928c02314307746b0281c6c9d672c03af
Talitrus <nguyenbryann@gmail.com>
2018-05-16 11:26:47 -0400

    change sequence table output to include table with SHA1 names

--- dada2_removechim.R
+++ dada2_removechim.R
@@ -1,4 +1,5 @@
 library(dada2); packageVersion("dada2")
+library(openssl)
 # Merge multiple runs (if necessary)
 st1 <- readRDS("BCS1_seqtab.rds")
 st4 <- readRDS("BCS4_seqtab.rds")
@@ -14,8 +15,11 @@ st.all <- mergeSequenceTables(st1, st4, st8, st9, st10, st11, st6, st3, st12)
 seqtab <- removeBimeraDenovo(st.all, method="consensus", multithread=TRUE)
 # Assign taxonomy
 # Write to disk
-saveRDS(seqtab, "seqtab_final.rds") # CHANGE ME to where you want sequence table saved
-seqtab <- readRDS("seqtab_final.rds")
+saveRDS(seqtab, "seqtab_final_original.rds") # CHANGE ME to where you want sequence table saved
+#seqtab <- readRDS("seqtab_final.rds")
+seqtab_sha1 <- seqtab
+colnames(seqtab_sha1) <- sha1(colnames(seqtab_sha1))
+saveRDS(seqtab_sha1, "seqtab_final.rds")
 uniquesToFasta(seqtab, fout = "uniques.fasta")
 #tax <- assignTaxonomy(seqtab, "../../Midori_COI/midori_COI_reformat.fasta", minBoot=70, multithread=TRUE, tryRC=TRUE)
 #saveRDS(tax, "midori70RC_tax_final.rds") # CHANGE ME ...
