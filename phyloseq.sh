53f4a2a5b4ba56354ed2ce7d52aa473d72836288
Bryan Nathan Nguyen <nguyenbryann@gmail.com>
2018-06-12 12:23:55 -0400

    increased memory requested by phyloseq.sh, shortened run time

--- phyloseq.sh
+++ phyloseq.sh
@@ -4,9 +4,9 @@
 #$ -j y
 #$ -N R_phyloseq
 #$ -o out_err_files/R_phyloseq_$JOB_ID.log
-#$ -q mThC.q
+#$ -q sThM.q
 #$ -pe mthread 6
-#$ -l mres=4G,h_data=4G,h_vmem=4G
+#$ -l mres=9G,h_data=9G,h_vmem=9G,himem
 
 module load tools/R
 Rscript BCS_phyloseq.R $NSLOTS
