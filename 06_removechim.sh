b4fa6eaca565b0e0d0aedee05a3866b7e99e4cbc
Bryan Nguyen <bnguyen@log001.colonialone.gwu.edu>
2019-05-20 14:36:42 -0400

    adjustments for C1 pegasus. fixed script to move files rather than delete

--- 06_removechim.sh
+++ 06_removechim.sh
@@ -3,10 +3,13 @@
 #SBATCH -o out_err_files/R_DADA2_chimera_%A_%a.out
 #SBATCH -e out_err_files/R_DADA2_chimera_%A_%a.err
 #SBATCH --nodes=1
-#SBATCH -t 7-00:00:00
-#SBATCH -p defq
+#SBATCH -t 1-00:00:00
+#SBATCH -p defq,short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=bnguyen@gwu.edu
 
-module load R/3.4.2
+module use /groups/cbi/modulefiles
+module load R/3.5.3
+module load vsearch
 Rscript dada2_removechim.R
+vsearch --fastx_filter uniques.fasta --relabel_sha1 --fastaout uniques_sha1.fasta
--- rename_postQC.R
+++ rename_postQC.R
@@ -1,14 +1,14 @@
 
-setwd("/groups/cbi/bryan/BCS_18S/")
+setwd("/groups/cbi/Users/bnguyen/bocas/BCS_18S/scripts")
 
 library(stringr)
 BCS_list <- list.dirs('..', recursive = FALSE)[grep("BCS", list.dirs('..', recursive = FALSE))]
 BCS_fnFs <- lapply(BCS_list, function(x) sort(list.files(x, pattern="final_barcode.+_2.fastq", full.names = TRUE, recursive = FALSE))) # The forward read is actually labeled as read 2 as a result of running Flexbar by feeding the reverse read in first.
 BCS_fnRs <- lapply(BCS_list, function(x) sort(list.files(x, pattern="final_barcode.+_1.fastq", full.names = TRUE, recursive = FALSE)))
-libkey <- read.delim("../key.txt", header = TRUE)
+libkey <- read.delim("helper_files/key.txt", header = TRUE)
 fnFs <- as.character(unlist(BCS_fnFs))
 fnRs <- as.character(unlist(BCS_fnRs))
-file_roots <- substr(fnFs,1,nchar(fnFs)-8)
+file_roots <- substr(fnFs,1,nchar(fnFs)-10) # "-10" if .gz, "-8" otherwise
 #make sure all unassigned barcode files moved out into a different folder.
 adapter <- as.integer(str_match(file_roots,"(?<=_S)[0-9]+(?=_)"))
 primer_tag <- as.integer(str_match(file_roots,"(?<=barcode_)[0-9]+"))
@@ -19,7 +19,7 @@ get_MLID <- function (lib_num, ad_num, prim_num) {
 }
 
 #Check for any mismatched forward/reverse files
-file_roots_R <- substr(fnRs,1,nchar(fnRs)-8)
+file_roots_R <- substr(fnRs,1,nchar(fnRs)-10)
 if (any(!(file_roots_R %in% file_roots)) | any(!(file_roots %in% file_roots_R))) {
   print("Mismatched post-Flexbar forward and reverse reads. No corresponding pair found for files:")
   print(which(!(file_roots_R %in% file_roots)))
@@ -33,5 +33,5 @@ read.df <- data.frame(fnFs, fnRs, file_roots, lib_number, adapter, primer_tag)
 read.df$MLID <- apply(read.df[,c('lib_number', 'adapter','primer_tag')], 1, function (x) get_MLID(x[1],x[2],x[3]))
 read.df$uniqID <- paste0('BCS',read.df$lib_number,'-',read.df$adapter,'-',read.df$primer_tag,'_',read.df$MLID)
   
-file.copy( from = as.character(read.df$fnFs), to = paste0('../consolidated/',read.df$uniqID,'_1.fastq'))
-file.copy( from = as.character(read.df$fnRs), to = paste0('../consolidated/',read.df$uniqID,'_2.fastq'))
+file.copy( from = as.character(read.df$fnFs), to = paste0('../consolidated/',read.df$uniqID,'_1.fastq.gz'))
+file.copy( from = as.character(read.df$fnRs), to = paste0('../consolidated/',read.df$uniqID,'_2.fastq.gz'))
